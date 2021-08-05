2345aa5c43ffc3541c227c92e9f4d42741eb4caa
Felipe Grassi Ribon <60076271+feribon@users.noreply.github.com>
2021-08-05 16:20:30 -0300

    Update solucao.js
    exercicios 1 a 10

--- solucao.js
+++ solucao.js
@@ -1092,24 +1092,111 @@ var listaProdutos = [
         }
     }
 ]
-
-function resolucao(){
-    ex1();
-    ex2();
-    ex3();
+function btn01() {
+    let ex = 0
+    for (item in listaProdutos) {
+        ex += listaProdutos[item].qtdEstoque
+    }
+    document.querySelector('#resp01').innerHTML = `${ex} itens em estoque`
 }
-
-function ex1(){
-    // meu desafio aqui é percorrer a lista toda e mostrar quantos produtos existem
-    console.log("total de produtos: "+listaProdutos.length);
+function btn02() {
+    let ex = 0
+    for (item in listaProdutos) {
+        if (listaProdutos[item].emDestaque === 'sim') {
+            ex += listaProdutos[item].qtdEstoque
+        }
+    }
+    document.querySelector('#resp02').innerHTML = `${ex} itens em destaque`
 }
-
-function ex2(){
-    //
+function btn03() {
+    let ex = 0
+    for (item in listaProdutos) {
+        if (listaProdutos[item].disponivel === 'sim') {
+            ex += listaProdutos[item].qtdEstoque
+        }
+    }
+    document.querySelector('#resp03').innerHTML = `${ex} itens disponiveis`
 }
-
-function ex3(){
-    //
-
+function btn04() {
+    let ex = 0
+    for (item in listaProdutos) {
+        if (listaProdutos[item].disponivel === 'sim' && listaProdutos[item].emDestaque === 'sim') {
+            ex += listaProdutos[item].qtdEstoque
+        }
+        document.querySelector('#resp04').innerHTML = `${ex} itens disponiveis e em destaque`
+    }
 }
-
+function btn05() {
+    let ex = 0
+    for (item in listaProdutos) {
+        if (listaProdutos[item].disponivel === 'sim') {
+            ex += listaProdutos[item].qtdEstoque * listaProdutos[item].preco
+        }
+        document.querySelector('#resp05').innerHTML = `Valor do inventário: R$${ex} `
+    }
+}
+function btn06() {
+    let preco = 0
+    let dep = ""
+    let prod = ""
+    let ex = 0
+    for (item in listaProdutos) {
+        if (listaProdutos[item].preco > ex) {
+            ex = listaProdutos[item].preco
+            prod = listaProdutos[item].descricao
+            dep = listaProdutos[item].departamento.nomeDepto
+            preco = listaProdutos[item].preco
+        }
+        document.querySelector('#resp06').innerHTML = `Produto: ${prod} </br> Preço: R$${preco.toFixed(2)} </br> Departamento: ${dep}`
+    }
+}
+function btn07() {
+    let preco = 0
+    let dep = ""
+    let prod = ""
+    let ex = listaProdutos[0].preco
+    for (item in listaProdutos) {
+        if (listaProdutos[item].preco < ex) {
+            ex = listaProdutos[item].preco
+            prod = listaProdutos[item].descricao
+            dep = listaProdutos[item].departamento.nomeDepto
+            preco = listaProdutos[item].preco
+        }
+        document.querySelector('#resp07').innerHTML = `Produto: ${prod} </br> Preço: R$${preco.toFixed(2)} </br> Departamento: ${dep}`
+    }
+}
+function btn08() {
+    let prod = ""
+    let ex = 0
+    for (item in listaProdutos) {
+        if (listaProdutos[item].disponivel === 'sim') {
+            if ((listaProdutos[item].preco * listaProdutos[item].qtdEstoque) > ex) {
+                ex = listaProdutos[item].preco * listaProdutos[item].qtdEstoque
+                prod = listaProdutos[item].descricao
+            }
+        }
+        document.querySelector('#resp08').innerHTML = prod
+    }
+}
+function btn09() {
+    let prod = ""
+    let ex = listaProdutos[0].preco * listaProdutos[0].qtdEstoque
+    for (item in listaProdutos) {
+        if (listaProdutos[item].disponivel === 'sim') {
+            if ((listaProdutos[item].preco * listaProdutos[item].qtdEstoque) < ex) {
+                ex = listaProdutos[item].preco * listaProdutos[item].qtdEstoque
+                prod = listaProdutos[item].descricao
+            }
+        }
+        document.querySelector('#resp09').innerHTML = prod
+    }
+}
+function btn10() {
+    let ex = 0
+    for (item in listaProdutos) {
+ex += listaProdutos[item].preco
+    }
+    let media = ex / listaProdutos.length
+    // console.log(ex, listaProdutos.length)
+    document.querySelector('#resp10').innerHTML = `R$${media.toFixed(2)}`
+}
